7d532f560fef0f04136dcc0ee8785c06f8f80578
taehoon <mysoul98@gmail.com>
2023-11-14 19:25:24 +0900

    feat: 버튼, 드롭다운 컴포넌트 및 tailwind config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#22a3cc',
+        primary: {
+          50: '#f3fafc',
+          100: '#e7f5f9',
+          200: '#c7e4f5',
+          300: '#a7d2f0',
+          400: '#67afe0',
+          500: '#2299cc',
+          600: '#1f87b2',
+          700: '#1a718f',
+          800: '#165c6c',
+          900: '#134c5a',
+        },
         semi_primary: '#bce3ef',
         secondary: '#bfd9ff',
         danger: '#e74c3c',
