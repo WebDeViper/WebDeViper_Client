2feb6dece1cc276a23ae3f984725a6eb73f6f7ba
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-11-16 20:07:21 +0900

    feat: redux 세팅 및 타입 지정

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { persistor, store } from './store/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,3 +1,4 @@
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import storage from 'redux-persist/lib/storage';
@@ -14,7 +15,6 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export type RootState = ReturnType<typeof rootReducer>;
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: getDefaultMiddleware =>
@@ -27,4 +27,8 @@ export const store = configureStore({
 
 export const persistor = persistStore(store);
 
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
