34354747e85e5ad035bdc12c3e05628dfe266e4d
taehoon <mysoul98@gmail.com>
2023-11-18 22:08:38 +0900

    fix: 스터디 페이지 이동 시 undefined 에러 수정

--- src/pages/StudyPage/StudyGroup/index.tsx
+++ src/pages/StudyPage/StudyGroup/index.tsx
@@ -43,40 +43,40 @@ export default function StudyGroup({ userId }: Props) {
         <h2 className="font-bold text-2xl">이런 스터디는 어떠세요?</h2>
       </div>
       <div className="absolute top-1 right-0">
-        <Button handleClick={handleCreateGroup}>스터디 만들기</Button>
+        <Button onClick={handleCreateGroup}>스터디 만들기</Button>
       </div>
       <div>
-        {!studyGroup.length && (
+        {!studyGroup.length ? (
           <div className="h-20 w-full flex flex-col justify-center p-2 shadow-lg rounded-lg">
             <p className="text-lg font-medium">아직 생성된 스터디그룹이 없어요!</p>
             <p>그룹을 생성해보세요!</p>
           </div>
+        ) : (
+          <Swiper
+            breakpoints={{
+              768: {
+                slidesPerView: 3,
+              },
+            }}
+            navigation={true}
+            keyboard={true}
+            modules={[Navigation, Pagination, Keyboard, Autoplay]}
+            slidesPerView={1}
+            spaceBetween={10}
+            autoplay={{ delay: 3000 }}
+            className="swiper_custom p-3 h-72"
+          >
+            {studyGroup?.map((item, index) => {
+              const { group_id } = item;
+
+              return (
+                <SwiperSlide key={index}>
+                  <GroupItem key={group_id} groupInfo={item} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
         )}
-
-        <Swiper
-          breakpoints={{
-            768: {
-              slidesPerView: 3,
-            },
-          }}
-          navigation={true}
-          keyboard={true}
-          modules={[Navigation, Pagination, Keyboard, Autoplay]}
-          slidesPerView={1}
-          spaceBetween={10}
-          autoplay={{ delay: 3000 }}
-          className="swiper_custom p-3 h-72"
-        >
-          {studyGroup?.map((item, index) => {
-            const { group_id } = item;
-
-            return (
-              <SwiperSlide key={index}>
-                <GroupItem key={group_id} groupInfo={item} />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
       </div>
     </section>
   );
