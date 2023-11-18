7df5c40a31005cf6c51acdb09da6005238344cfa
taehoon <mysoul98@gmail.com>
2023-11-18 18:56:38 +0900

    feat: 스터디 생성 페이지

--- src/pages/CreateGroupPage/index.tsx
+++ src/pages/CreateGroupPage/index.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
 import { Button } from '../../components/common/Button';
-import { useForm } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { API } from '../../utils/axios';
 import { useNavigate } from 'react-router-dom';
 import { SimpleDropdown } from '../../components/common/Dropdown';
@@ -20,12 +19,8 @@ interface GroupFormType {
 export default function CreateGroupPage() {
   const navigate = useNavigate();
 
-  // 드롭다운 선택 후 input에 값을 넣기 위한 state
-  const [groupCategory, setGroupCategory] = useState<string | undefined>('');
-  const [groupGoalTime, setGroupGoalTime] = useState<string | undefined>('');
-
   // react hook form 사용하기
-  const { register, handleSubmit } = useForm<GroupFormType>({
+  const { register, handleSubmit, setValue, watch } = useForm<GroupFormType>({
     mode: 'onSubmit',
     defaultValues: {
       group_name: '',
@@ -39,7 +34,7 @@ export default function CreateGroupPage() {
   });
 
   // form 제출
-  const handleOnSubmit = handleSubmit(async data => {
+  const onSubmit: SubmitHandler<GroupFormType> = async data => {
     console.log('폼 제출 데이터 :: ', data);
     try {
       const res = await API.post('/group/studyGroup', data, {
@@ -55,7 +50,7 @@ export default function CreateGroupPage() {
     } catch (err) {
       console.error(err);
     }
-  });
+  };
 
   // 드롭다운으로 input 값 설정하기
   const handleSelectCategory = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -63,7 +58,7 @@ export default function CreateGroupPage() {
     const targetElement = e.currentTarget;
     console.log('카테고리 선택 잘 되나요?', targetElement.textContent);
     if (targetElement.textContent) {
-      setGroupCategory(targetElement.textContent);
+      setValue('group_category', targetElement.textContent);
     }
   };
 
@@ -72,7 +67,24 @@ export default function CreateGroupPage() {
     const targetElement = e.currentTarget;
     console.log('그룹시간 선택 잘 되나요?', targetElement.textContent);
     if (targetElement.textContent) {
-      setGroupGoalTime(targetElement.textContent);
+      setValue('dailyGoalTime', targetElement.textContent);
+    }
+  };
+
+  // 그룹 이미지 유효성 검사
+  const handleGroupImg = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile: File | null = e.target.files && e.target.files[0];
+
+    if (selectedFile) {
+      const allowedExtensions = /(\.png|\.jpg|\.jpeg)$/i;
+
+      if (!allowedExtensions.exec(selectedFile.name)) {
+        alert('올바른 파일 확장자를 사용해주세요.');
+        // 선택 파일 초기화
+        e.target.value = '';
+      }
+    } else {
+      setValue('group_image', selectedFile);
     }
   };
 
@@ -81,16 +93,26 @@ export default function CreateGroupPage() {
       <div className="groupHeader mb-5 flex justify-between items-center">
         <h2 className="font-bold text-2xl">스터디 그룹 생성</h2>
       </div>
-      <form className="groupInfoWrap flex flex-col gap-3 mb-5" onSubmit={handleOnSubmit}>
-        <div className="buttonWrap self-end">
-          <Button>생성</Button>
-        </div>
-        <div className="groupImage flex justify-between">
+      <form className="groupInfoWrap flex flex-col gap-3 mb-5" onSubmit={handleSubmit(onSubmit)}>
+        <div className="groupImage flex justify-between items-center">
           <div>그룹이미지</div>
-          <label className="w-30 h-30 bg-primary-300"></label>
-          <input type="file" {...register('group_image')} id="group_image" />
+          <label
+            className="w-auto h-[200px] bg-primary-100 flex flex-col justify-center items-center text-center p-2 rounded-lg"
+            htmlFor="group_image"
+          >
+            <span className="font-semibold">그룹 이미지 등록</span>
+            <span className="text-sm">(확장자: png, jpg, jpeg / 용량 1MB 이하)</span>
+          </label>
+          <input
+            type="file"
+            {...register('group_image')}
+            id="group_image"
+            className="hidden"
+            accept="image/png, image/jpg, image/jpeg"
+            onChange={handleGroupImg}
+          />
         </div>
-        <div className="groupName flex justify-between">
+        <div className="groupName flex justify-between items-center">
           <label>그룹명</label>
           <input
             type="text"
@@ -99,23 +121,21 @@ export default function CreateGroupPage() {
             })}
           />
         </div>
-        <div className="groupCategory flex justify-between">
+        <div className="groupCategory flex justify-between items-center">
           <label>카테고리</label>
           <SimpleDropdown
-            title={groupCategory ? groupCategory : '카테고리'}
+            title={watch('group_category') ? watch('group_category') : '카테고리'}
             items={categories}
             handleClick={handleSelectCategory}
           ></SimpleDropdown>
           <input
-            className="opacity-0"
-            type="text"
+            type="hidden"
             {...register('group_category', {
               required: '카테고리를 선택해주세요!',
             })}
-            value={groupCategory}
           />
         </div>
-        <div className="groupDescription flex justify-between">
+        <div className="groupDescription flex justify-between items-center">
           <label>그룹 설명</label>
           <input type="text" {...register('group_description')} />
         </div>
@@ -128,26 +148,25 @@ export default function CreateGroupPage() {
             })}
           />
         </div>
-        <div className="groupDailyGoalTime flex justify-between">
+        <div className="groupDailyGoalTime flex justify-between items-center">
           <label>목표시간</label>
           <SimpleDropdown
-            title="목표시간"
+            title={watch('dailyGoalTime') ? watch('dailyGoalTime') : '목표시간'}
             items={['2', '4', '6', '8', '10']}
             handleClick={handleSelectTime}
           ></SimpleDropdown>
           <input
-            type="text"
-            className="opacity-0"
+            type="hidden"
             {...register('dailyGoalTime', {
               required: '그룹 목표시간을 선택해주세요!',
             })}
-            value={groupGoalTime}
           />
         </div>
-        <div className="GroupIsCameraOn flex justify-between">
+        <div className="GroupIsCameraOn flex justify-between items-center mb-5">
           <label>줌 여부</label>
           <input type="checkbox" {...register('isCameraOn')} />
         </div>
+        <Button>생성</Button>
       </form>
     </div>
   );
