9338147bb0bc444e618d3e399aed1e0e1ba52dfc
seokmin <msm0748@gmail.com>
2023-11-21 16:04:38 +0900

    feat: 알림 읽기 기능

--- src/pages/AlarmPage/index.tsx
+++ src/pages/AlarmPage/index.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from 'react';
-import { useAppSelector } from '../../store/store';
+import { useAppDispatch, useAppSelector } from '../../store/store';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import { deleteAlarm } from '../../store/thunkFunctions';
 
 export default function AlarmPage() {
   const [alarmList, setAlarmList] = useState<Alarm[]>([]);
   const alarmMessage = useAppSelector(state => state.user.userInfo.alarmMessage);
+
+  const dispatch = useAppDispatch();
+
+  const handleDeleteAlarm = async (id: string) => {
+    const data = { _id: id };
+    dispatch(deleteAlarm(data));
+  };
 
   useEffect(() => {
     setAlarmList(alarmMessage);
@@ -17,7 +25,12 @@ export default function AlarmPage() {
           if (value.notification_kind === 'new_notice') {
             return (
               <div key={value._id}>
-                <Link to={`/notice/${value.content_id}`}>
+                <Link
+                  to={`/notice/${value.content_id}`}
+                  onClick={() => {
+                    handleDeleteAlarm(value._id);
+                  }}
+                >
                   <div className="flex gap-4">
                     <span>{value.content}</span>
                     <span>보낸 사람 : {value.user_id === 'admin' ? '관리자' : value.user_id}</span>
