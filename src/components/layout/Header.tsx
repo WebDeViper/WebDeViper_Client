9646d4838e65eeaf8e83fc3c721b4a51bdcb9729
seokmin <msm0748@gmail.com>
2023-11-23 16:19:18 +0900

    feat: 헤더바 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import LoginModal from '../LoginModal';
 import { useAppDispatch, useAppSelector } from '../../store/store';
 import { logout } from '../../store/userSlice';
 import { GoBell } from 'react-icons/go';
 import { HiMiniBars3 } from 'react-icons/hi2';
+import useModalToggle from '../../hooks/useModalToggle';
+import { IoCloseSharp } from 'react-icons/io5';
+import { useMediaQuery } from 'react-responsive';
 
 const navigation = [
   { name: '공부하기', href: '/study', current: false },
@@ -19,16 +22,28 @@ export default function Header() {
   const alarmMessage = useAppSelector(state => state.user.userInfo.alarmMessage);
   const [modalOpen, setModalOpen] = useState(false);
   const dispatch = useAppDispatch();
+  const [isProfileOpen, selectProfileRef, toggleProfileOpen] = useModalToggle();
+  const [isMobileMenu, setIsMobileMenu] = useState(false);
+  const isPC = useMediaQuery({ query: '(min-width: 768px)' });
 
   console.log(alarmMessage);
   const handleModalOpen = () => {
     setModalOpen(true);
   };
 
+  const handleToggleMobile = () => {
+    setIsMobileMenu(prev => !prev);
+  };
+
   const handleLogout = () => {
+    toggleProfileOpen();
     dispatch(logout());
     setModalOpen(false);
   };
+
+  useEffect(() => {
+    setIsMobileMenu(false);
+  }, [isPC]);
 
   const user = {
     nickname: userInfo.nickName,
@@ -38,9 +53,11 @@ export default function Header() {
 
   return (
     <header className="bg-gray-800">
+      {/* 로그인 모달 */}
       {!isAuth && <LoginModal open={modalOpen} onClose={setModalOpen} />}
+
       <div className="container">
-        <div className="flex h-16 items-center justify-between">
+        <div className="flex h-16 items-center justify-between relative">
           <div className="flex items-center">
             <div className="flex-shrink-0">
               <Link to={'/'}>
@@ -61,25 +78,48 @@ export default function Header() {
                 ))}
               </div>
             </div>
-            {/* mobile 좌측 */}
-            <div className="md:hidden">모바일 일 때 여기 코드</div>
           </div>
           {/* pc 우측 */}
           <div className="hidden md:block">
-            <div className="flex items-center md:ml-6 gap-4">
+            <div className="flex items-center md:ml-6 gap-4 relative">
               {/* 로그인 사용자만 보일 수 있게 처리 */}
               {isAuth ? (
                 <>
-                  <button className="text-2xl text-gray-400 hover:text-white p-1">
+                  <button className="text-2xl text-gray-400 hover:text-white p-1 focus:outline-none rounded-full">
                     <GoBell />
                   </button>
-                  <button className="p-1">
+                  <button className="p-1" onClick={toggleProfileOpen}>
                     <img
                       className="h-8 w-8 rounded-full"
                       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5allhVIDcWziGtl4Rhu3Ccm5SkkTi50ageg&usqp=CAU"
                       alt="profile"
                     />
                   </button>
+                  {/* 프로필 클릭 */}
+                  {isProfileOpen && (
+                    <div
+                      className="absolute z-10 right-0 shadow-lg rounded-md bg-white mt-12 top-0 py-1 w-48"
+                      ref={selectProfileRef}
+                    >
+                      <div className="hover:bg-gray-100">
+                        <Link
+                          to="/profile"
+                          className="block px-4 py-2 text-sm text-gray-700"
+                          onClick={toggleProfileOpen}
+                        >
+                          프로필
+                        </Link>
+                      </div>
+                      <div className="hover:bg-gray-100">
+                        <button
+                          onClick={handleLogout}
+                          className="block w-full text-left px-4 py-2 text-sm text-gray-700"
+                        >
+                          로그아웃
+                        </button>
+                      </div>
+                    </div>
+                  )}
                 </>
               ) : (
                 <>
@@ -101,11 +141,95 @@ export default function Header() {
           </div>
           {/* mobile 우측 */}
           <div className="md:hidden">
-            <button className="text-2xl rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white">
-              <HiMiniBars3 />
+            <button
+              onClick={handleToggleMobile}
+              className="text-2xl rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none"
+            >
+              {isMobileMenu ? <IoCloseSharp /> : <HiMiniBars3 />}
             </button>
           </div>
         </div>
+        {/* 모바일 메뉴 */}
+      </div>
+      <div className="md:hidden">
+        {isMobileMenu && (
+          <>
+            <div className="container">
+              <div className="-ml-3 pb-3 pt-2">
+                {navigation.map(item => (
+                  <Link
+                    to={item.href}
+                    key={item.name}
+                    className="text-white block rounded-md py-2 text-base font-medium hover:bg-gray-700 px-3"
+                    onClick={() => setIsMobileMenu(false)}
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+
+            <div className="border-t border-gray-700">
+              <div className="container">
+                <div className="-ml-3 pb-3 pt-2">
+                  {/* 로그인 프로필 */}
+                  {isAuth ? (
+                    <>
+                      <div className="flex justify-between px-3 py-3">
+                        <div className="flex gap-3 items-center">
+                          <img
+                            className="h-10 w-10 rounded-full"
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5allhVIDcWziGtl4Rhu3Ccm5SkkTi50ageg&usqp=CAU"
+                            alt="profile"
+                          />
+
+                          <div className="flex flex-col gap-1">
+                            <div className="text-base font-medium leading-none text-white">{user.nickname}</div>
+                            <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                          </div>
+                        </div>
+                        <button className="text-2xl text-gray-400 hover:text-white p-1 focus:outline-none rounded-full">
+                          <GoBell />
+                        </button>
+                      </div>
+                      <div>
+                        <Link
+                          to="/profile"
+                          className="text-white block rounded-md py-2 text-base font-medium hover:bg-gray-700 px-3"
+                          onClick={() => setIsMobileMenu(false)}
+                        >
+                          프로필
+                        </Link>
+                        <button
+                          onClick={handleLogout}
+                          className="text-white block rounded-md py-2 text-base font-medium hover:bg-gray-700 px-3 w-full text-left"
+                        >
+                          로그아웃
+                        </button>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <button
+                        onClick={handleModalOpen}
+                        className="text-white block rounded-md py-2 text-base font-medium hover:bg-gray-700 px-3 w-full text-left"
+                      >
+                        로그인
+                      </button>
+                      <Link
+                        to="/signup"
+                        className="text-white block rounded-md py-2 text-base font-medium hover:bg-gray-700 px-3"
+                        onClick={() => setIsMobileMenu(false)}
+                      >
+                        회원가입
+                      </Link>
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </header>
   );
