0a1d3782d9902ba3ab2a97f4d3bb8b895fc00403
seokmin <msm0748@gmail.com>
2023-11-24 21:12:15 +0900

    feat: 할 일 생성 구현

--- src/pages/CalendarPage/AddTodoModal.tsx
+++ src/pages/CalendarPage/AddTodoModal.tsx
@@ -3,16 +3,46 @@ import Modal from '../../components/common/Modal';
 import CustomDatePicker from './DatePicker';
 import { TodoSelectedDateValue } from './index';
 import { Button } from '../../components/common/Button';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import moment from 'moment';
+import { API } from '../../utils/axios';
 
 interface Props {
   show: boolean;
-  onClose: React.Dispatch<React.SetStateAction<boolean>>;
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
   selectedDate: TodoSelectedDateValue;
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-export default function AddTodoModal({ show, selectedDate, onClose }: Props) {
+export type TodoFormValues = {
+  title: string;
+  startHour: string;
+  startMinute: string;
+  endHour: string;
+  endMinute: string;
+  content: string;
+};
+
+const defaultValues = {
+  title: '',
+  startHour: '18',
+  startMinute: '00',
+  endHour: '18',
+  endMinute: '00',
+  content: '',
+};
+
+export default function AddTodoModal({ show, selectedDate, setTodos, setOpenModal }: Props) {
   const [startDate, setStartDate] = useState<Date>(new Date());
   const [endDate, setEndDate] = useState<Date>(new Date());
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ defaultValues });
 
   useEffect(() => {
     setStartDate(selectedDate as Date);
@@ -33,44 +63,114 @@ export default function AddTodoModal({ show, selectedDate, onClose }: Props) {
     setEndDate(date);
   };
 
+  const handleCancel = () => {
+    setOpenModal(false);
+    reset();
+  };
+  const onSubmit: SubmitHandler<TodoFormValues> = async data => {
+    const ERR_MESSAGE = '시작 시간이 종료 시간보다 빨라야 합니다.';
+    const startHour = Number(data.startHour);
+    const endHour = Number(data.endHour);
+    const startMinute = Number(data.startMinute);
+    const endMinute = Number(data.endMinute);
+
+    if (startDate === endDate) {
+      if (startHour > endHour) return toast.info(ERR_MESSAGE, { type: toast.TYPE.ERROR });
+      if (startHour === endHour) {
+        if (startMinute > endMinute) return toast.error(ERR_MESSAGE);
+      }
+    }
+    console.log(data);
+
+    try {
+      const momentStartDate = moment(startDate).format('yyyy, MM, DD');
+      const momentEndDate = moment(endDate).format('yyyy, MM, DD');
+
+      const newStateDate = new Date(momentStartDate);
+      newStateDate.setHours(startHour);
+      newStateDate.setMinutes(startMinute);
+
+      const newEndDate = new Date(momentEndDate);
+      newEndDate.setHours(endHour);
+      newEndDate.setMinutes(endMinute);
+
+      const body = {
+        title: data.title,
+        content: data.content,
+        start_time: newStateDate,
+        end_time: newEndDate,
+      };
+      const response = await API.post('/todo_list', body);
+      const newData = response.data.result;
+      console.log(newData);
+      setTodos(prev => [...prev, newData]);
+      setOpenModal(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <Modal show={show} onClose={onClose}>
-      <div className="space-y-6 sm:w-96 w-80">
-        <h3 className="text-xl font-medium text-gray-900 dark:text-white">일정</h3>
-        <div>
-          <label htmlFor="title" className="block font-medium leading-6 text-gray-900">
-            제목
-          </label>
-          <div className="mt-2">
-            <input
-              id="title"
-              type="title"
-              autoComplete="title"
-              required
-              className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-            />
+    <Modal show={show} onClose={setOpenModal}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-6 sm:w-96 w-80">
+          <h3 className="text-xl font-medium text-gray-900 dark:text-white">일정</h3>
+          <div>
+            <label htmlFor="title" className="block font-medium leading-6 text-gray-900">
+              제목
+            </label>
+            <div className="mt-2">
+              <input
+                id="title"
+                type="title"
+                autoComplete="title"
+                required
+                className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                {...register('title', {
+                  required: '제목을 입력하세요.',
+                })}
+              />
+            </div>
+            {errors?.title && <p className="text-danger">{errors.title.message}</p>}
+          </div>
+          <CustomDatePicker
+            label="시작"
+            selectedDate={startDate}
+            handleChange={handleStartDateChange}
+            register={register}
+            hourName="startHour"
+            minuteName="startMinute"
+            errors={errors}
+          />
+          <CustomDatePicker
+            label="종료"
+            selectedDate={endDate}
+            handleChange={handleEndDateChange}
+            register={register}
+            hourName="endHour"
+            minuteName="endMinute"
+            errors={errors}
+          />
+          <div>
+            <label htmlFor="content" className="block font-medium leading-6 text-gray-900">
+              내용
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="content"
+                rows={5}
+                className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                {...register('content')}
+              />
+            </div>
+          </div>
+          <div className="flex gap-3">
+            <Button type="submit">확인</Button>
+            <Button color="white" type="button" onClick={handleCancel}>
+              취소
+            </Button>
           </div>
         </div>
-        <CustomDatePicker label="시작" selectedDate={startDate} handleChange={handleStartDateChange} />
-        <CustomDatePicker label="종료" selectedDate={endDate} handleChange={handleEndDateChange} />
-        <div className="col-span-full">
-          <label htmlFor="content" className="block font-medium leading-6 text-gray-900">
-            내용
-          </label>
-          <div className="mt-2">
-            <textarea
-              id="content"
-              rows={5}
-              className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-              defaultValue={''}
-            />
-          </div>
-        </div>
-        <div className="flex gap-3">
-          <Button>확인</Button>
-          <Button color="white">취소</Button>
-        </div>
-      </div>
+      </form>
     </Modal>
   );
 }
--- src/pages/CalendarPage/DatePicker.tsx
+++ src/pages/CalendarPage/DatePicker.tsx
@@ -1,14 +1,39 @@
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { ko } from 'date-fns/esm/locale';
+import { FieldErrors, UseFormRegister } from 'react-hook-form';
+import { TodoFormValues } from './AddTodoModal';
 
 interface Props {
   label: '시작' | '종료';
   selectedDate: Date;
   handleChange: (date: Date) => void;
+  register: UseFormRegister<TodoFormValues>;
+  hourName: 'startHour' | 'endHour';
+  minuteName: 'startMinute' | 'endMinute';
+  errors: FieldErrors<TodoFormValues>;
 }
 
-export default function CustomDatePicker({ label, selectedDate, handleChange }: Props) {
+export default function CustomDatePicker({
+  label,
+  selectedDate,
+  handleChange,
+  register,
+  hourName,
+  minuteName,
+  errors,
+}: Props) {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // 입력값이 변경될 때 숫자만 남도록 처리
+    const inputValue = e.target.value.replace(/[^0-9]/g, '');
+    e.target.value = inputValue;
+  };
+
+  const isWithinMaxValue = (value: string, max: number): boolean => {
+    const numericValue = parseFloat(value);
+    return !isNaN(numericValue) && numericValue <= max;
+  };
+
   return (
     <div>
       <div className="">
@@ -27,15 +52,32 @@ export default function CustomDatePicker({ label, selectedDate, handleChange }: 
                 type="text"
                 className="w-16 text-center rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                 placeholder="18"
+                {...register(hourName, {
+                  validate: {
+                    withinMaxValue: value => isWithinMaxValue(value, 23) || '시간의 최대값은 23입니다.',
+                  },
+                })}
+                maxLength={2}
+                onInput={handleInputChange}
               />
               <span> : </span>
               <input
                 type="text"
                 className="w-16 text-center rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
                 placeholder="00"
+                maxLength={2}
+                onInput={handleInputChange}
+                {...register(minuteName, {
+                  validate: {
+                    withinMaxValue: value => isWithinMaxValue(value, 59) || '분의 최대값은 59입니다.',
+                  },
+                })}
               />
             </div>
           </div>
+          {errors[hourName]?.message || errors[minuteName]?.message ? (
+            <p className="text-danger text-right">{errors[hourName]?.message || errors[minuteName]?.message}</p>
+          ) : null}
         </div>
       </div>
     </div>
