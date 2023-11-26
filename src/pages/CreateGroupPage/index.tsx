9e40bbebbdf3e773ae0b6b1b72fc700148d87974
seokmin <msm0748@gmail.com>
2023-11-27 02:51:46 +0900

    design: 그룹 생성 페이지 디자인 수정

--- src/pages/CreateGroupPage/index.tsx
+++ src/pages/CreateGroupPage/index.tsx
@@ -6,6 +6,7 @@ import { SimpleDropdown } from '../../components/common/Dropdown';
 import categories from '../../data/category';
 import './index.css';
 import { useState } from 'react';
+import SelectMenu from '../../components/common/SelectMenu';
 
 interface GroupFormType {
   name: string;
@@ -57,7 +58,6 @@ export default function CreateGroupPage() {
   const handleSelectCategory = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
     const targetElement = e.currentTarget;
-    console.log('카테고리 선택 잘 되나요?', targetElement.textContent);
     if (targetElement.textContent) {
       setValue('category', targetElement.textContent);
     }
@@ -66,7 +66,6 @@ export default function CreateGroupPage() {
   const handleSelectTime = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
     const targetElement = e.currentTarget;
-    console.log('그룹시간 선택 잘 되나요?', targetElement.textContent);
     if (targetElement.textContent) {
       setValue('dailyGoalTime', targetElement.textContent);
     }
@@ -92,93 +91,115 @@ export default function CreateGroupPage() {
     }
   };
 
-  const [imgUrl, setImgUrl] = useState<string>('');
+  const [imgUrl, setImgUrl] = useState('');
 
   return (
-    <div className="createGroupWrap container flex flex-col justify-center">
-      <div className="groupHeader mb-5 flex justify-between items-center">
-        <h2 className="font-bold text-2xl">스터디 그룹 생성</h2>
-      </div>
-      <form className="groupInfoWrap flex flex-col gap-3 mb-5" onSubmit={handleSubmit(onSubmit)}>
-        <div className="groupImage flex justify-between items-center">
-          <div>그룹이미지</div>
-          {!watch('groupImgFile') ? (
-            <label
-              className="w-auto h-[200px] bg-primary-100 flex flex-col justify-center items-center text-center p-2 rounded-lg"
-              htmlFor="groupImgFile"
-            >
-              <span className="font-semibold">그룹 이미지 등록</span>
-              <span className="text-sm">(확장자: png, jpg, jpeg / 용량 1MB 이하)</span>
-            </label>
-          ) : (
-            <img src={imgUrl} className="w-auto h-[200px] p-2 rounded-lg border-2" />
-          )}
+    <div className="container">
+      <div className="lg:w-[720px] mx-auto flex flex-col justify-center">
+        <div className="mb-5 flex justify-between items-center">
+          <h2 className="font-bold text-2xl">스터디 그룹 생성</h2>
+        </div>
+        <form className="flex flex-col gap-7 mb-5" onSubmit={handleSubmit(onSubmit)}>
+          <div className="relative">
+            <div className="md:absolute font-medium">대표 이미지</div>
+            <div className="md:pl-40 mt-3 md:mt-0">
+              {!watch('groupImgFile') ? (
+                <label
+                  className="md:w-[280px] w-full h-[200px] bg-primary-100 flex flex-col justify-center items-center text-center p-2 rounded-lg"
+                  htmlFor="groupImgFile"
+                >
+                  <span className="font-semibold">그룹 이미지 등록</span>
+                  <span className="text-sm">(확장자: png, jpg, jpeg / 용량 1MB 이하)</span>
+                </label>
+              ) : (
+                <img src={imgUrl} className="md:w-[280px] w-full h-[200px] rounded-lg border-2" />
+              )}
+            </div>
 
-          <input
-            type="file"
-            {...register('groupImgFile')}
-            id="groupImgFile"
-            className="hidden"
-            accept="image/png, image/jpg, image/jpeg"
-            onChange={handleGroupImg}
-          />
-        </div>
-        <div className="groupName flex justify-between items-center">
-          <label>그룹명</label>
-          <input
-            type="text"
-            {...register('name', {
-              required: '그룹명을 작성해주세요!',
-            })}
-          />
-        </div>
-        <div className="groupCategory flex justify-between items-center">
-          <label>카테고리</label>
-          <SimpleDropdown
-            title={watch('category') ? watch('category') : '카테고리'}
-            items={categories}
-            handleClick={handleSelectCategory}
-          ></SimpleDropdown>
-          <input
-            type="hidden"
-            {...register('category', {
-              required: '카테고리를 선택해주세요!',
-            })}
-          />
-        </div>
-        <div className="groupDescription flex justify-between items-center">
-          <label>그룹 설명</label>
-          <input type="text" {...register('description')} />
-        </div>
-        <div className="groupMaximumMember flex justify-between">
-          <label>모집인원</label>
-          <input
-            type="number"
-            {...register('maximumNumberMember', {
-              required: '그룹 모집인원을 선택해주세요!',
-            })}
-          />
-        </div>
-        <div className="groupDailyGoalTime flex justify-between items-center">
-          <label>목표시간</label>
-          <SimpleDropdown
-            title={watch('dailyGoalTime') ? watch('dailyGoalTime') : '목표시간'}
-            items={['2', '4', '6', '8', '10']}
-            handleClick={handleSelectTime}
-          ></SimpleDropdown>
-          <input
-            type="hidden"
-            {...register('dailyGoalTime', {
-              required: '그룹 목표시간을 선택해주세요!',
-            })}
-          />
-        </div>
-        {/* <div className="GroupIsCameraOn flex justify-between items-center mb-5">
-          <label>줌 여부</label>
-          <input type="checkbox" {...register('isCameraOn')} />
-        </div> */}
-        <Button type={'submit'}>생성</Button>
-      </form>
+            <input
+              type="file"
+              {...register('groupImgFile')}
+              id="groupImgFile"
+              className="hidden"
+              accept="image/png, image/jpg, image/jpeg"
+              onChange={handleGroupImg}
+            />
+          </div>
+          <div className="relative">
+            <label className="md:absolute font-medium">스터디 이름</label>
+            <div className="md:pl-40 mt-3 md:mt-0">
+              <input
+                type="text"
+                {...register('name', {
+                  required: '스터디 이름을 작성해주세요!',
+                })}
+                className="w-full border rounded-md py-1 pl-3"
+                placeholder="스터디 이름을 입력하세요."
+              />
+            </div>
+          </div>
+          <div className="relative">
+            <label className="md:absolute font-medium">카테고리</label>
+            <div className="md:pl-40 mt-3 md:mt-0">
+              <SimpleDropdown
+                title={watch('category') ? watch('category') : '카테고리'}
+                items={categories}
+                handleClick={handleSelectCategory}
+              />
+            </div>
+            <input
+              type="hidden"
+              {...register('category', {
+                required: '카테고리를 선택해주세요!',
+              })}
+            />
+          </div>
+          <div>
+            <label className="font-medium">그룹 설명</label>
+            <div className="mt-3">
+              <textarea
+                className="w-full border rounded-md py-1 pl-3"
+                rows={6}
+                {...register('description')}
+                placeholder="스터디 규칙, 공지 사항 등을 입력해주세요."
+              />
+            </div>
+          </div>
+          <div className="relative">
+            <label className="md:absolute font-medium">목표시간</label>
+            <div className="md:pl-40 mt-3 md:mt-0">
+              <SimpleDropdown
+                title={watch('dailyGoalTime') ? watch('dailyGoalTime') : '목표시간'}
+                items={['2', '4', '6', '8', '10']}
+                handleClick={handleSelectTime}
+              />
+            </div>
+            <input
+              type="hidden"
+              {...register('dailyGoalTime', {
+                required: '그룹 목표시간을 선택해주세요!',
+              })}
+            />
+          </div>
+          <div className="relative">
+            <label className="md:absolute font-medium">모집인원</label>
+            <div className="md:pl-40 mt-3 md:mt-0">
+              <input
+                type="number"
+                {...register('maximumNumberMember', {
+                  required: '그룹 모집인원을 선택해주세요!',
+                })}
+                className="w-full border rounded-md py-1 pl-3 pr-2"
+                max={50}
+                maxLength={2}
+                placeholder="50명 이하로 입력해주세요."
+              />
+            </div>
+          </div>
+
+          <Button type="submit">생성</Button>
+        </form>
+      </div>
     </div>
   );
 }
