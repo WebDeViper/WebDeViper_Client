dfae776d1fed13f8657f8192d3316ae0c62a5afa
Taehoon <mysoul98@gmail.com>
2023-11-27 21:54:03 +0900

    feat: 프로필 페이지 요청 수락/거절 api 연결

--- src/pages/ProfilePage/GroupInfo/index.tsx
+++ src/pages/ProfilePage/GroupInfo/index.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import GroupRequest from './GroupRequest';
 import MyGroupRequest from './MyGroupRequest';
 import { API } from '../../../utils/axios';
-// import { Card } from 'flowbite-react';
 
 export default function GroupInfo() {
   const [myOwnGroup, setMyOwnGroup] = useState([]);
@@ -10,14 +9,15 @@ export default function GroupInfo() {
 
   useEffect(() => {
     const getGroups = async () => {
-      const res = await API.get('/group/studyGroups/users');
-      console.log(res.data);
-      const filteredGroup = res.data.study_groups;
+      const res = await API.get('/group/getJoinRequest');
+      console.log('내 그룹에 신청중인 사람들 :: ', res.data);
+      const filteredGroup = res.data.groups;
       setMyOwnGroup(filteredGroup);
     };
     const getPendingGroups = async () => {
       try {
         const res = await API.get('/group/pendingGroups');
+        console.log('팬딩 그룹 서버 요청 :: ', res.data);
         setPendingGroups(res.data.pendingGroups); // 서버에서 받은 데이터를 설정
       } catch (error) {
         console.error('Error fetching pending groups:', error);
@@ -27,6 +27,7 @@ export default function GroupInfo() {
     getGroups();
     getPendingGroups();
   }, []);
+
   console.log('그룹 가입 요청 어쩌구', myOwnGroup);
   console.log('내 신청 중 그룹', pendingGroups);
 
@@ -34,28 +35,22 @@ export default function GroupInfo() {
     <div>
       <section>
         <h2>그룹 가입 요청</h2>
-        {/* <Card>요청이 없습니다.</Card> */}
         <div className="myOwnGroupWrap flex flex-wrap md:gap-1 md:mb-5 mb-1">
-          {/* {myOwnGroup.map(
-            group =>
-              group.join_requests.length > 0 && (
-                <GroupRequest
-                  key={group._id}
-                  requests={group.join_requests}
-                  groupName={group.group_name}
-                  groupId={group._id}
-                />
-              )
-          )} */}
+          {myOwnGroup?.map((group: any) => (
+            <GroupRequest key={group.group_id} group={group} />
+          ))}
         </div>
       </section>
       <section>
         <h2>신청중인 그룹</h2>
         <div className="myPendingGroupWrap flex flex-wrap md:gap-1">
-          {pendingGroups.length > 0 &&
+          {pendingGroups.length > 0 ? (
             pendingGroups.map((group, index) => (
               <MyGroupRequest key={index} groupInfo={group} setPendingGroups={setPendingGroups} />
-            ))}
+            ))
+          ) : (
+            <div>요청이 없습니다.</div>
+          )}
         </div>
       </section>
     </div>
