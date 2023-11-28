f00b0e1e9f49652cfc42df6c91ddab0b5d83af90
Taehoon <mysoul98@gmail.com>
2023-11-28 14:46:26 +0900

    refactor: 카카오 로그인만 남기는걸로 수정

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -4,8 +4,8 @@ import { loginUser } from '../store/thunkFunctions';
 import Modal from './common/Modal';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { RiKakaoTalkFill } from 'react-icons/ri';
-import { FcGoogle } from 'react-icons/fc';
-import { SiNaver } from 'react-icons/si';
+// import { FcGoogle } from 'react-icons/fc';
+// import { SiNaver } from 'react-icons/si';
 import { redirectUrl } from '../utils/redirectUrl';
 
 type FormValues = {
@@ -41,22 +41,22 @@ export default function LoginModal({ open, onClose }: ModalProps) {
   const KAKAO_REDIRECT_URI = redirectUrl('kakao');
   const KAKAO_LOGIN_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
-  const NAVER_CLIENT_ID = import.meta.env.VITE_NAVER_CLIENT_ID;
-  const NAVER_STATE = import.meta.env.VITE_NAVER_STATE;
-  const NAVER_REDIRECT_URI = redirectUrl('naver');
-  const NAVER_LOGIN_URI = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_REDIRECT_URI}&state=${NAVER_STATE}`;
+  // const NAVER_CLIENT_ID = import.meta.env.VITE_NAVER_CLIENT_ID;
+  // const NAVER_STATE = import.meta.env.VITE_NAVER_STATE;
+  // const NAVER_REDIRECT_URI = redirectUrl('naver');
+  // const NAVER_LOGIN_URI = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_REDIRECT_URI}&state=${NAVER_STATE}`;
 
-  const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-  const GOOGLE_REDIRECT_URL = redirectUrl('google');
-  const GOOGLE_LOGIN_URI = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URL}&response_type=code&scope=email profile`;
+  // const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+  // const GOOGLE_REDIRECT_URL = redirectUrl('google');
+  // const GOOGLE_LOGIN_URI = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URL}&response_type=code&scope=email profile`;
 
   // style
   // const kakaoColor = '#FEE500';
   // const googleColor = '#fefefe';
   // const naverColor = '#2DB400';
   const kakaoStyle = `w-full mb-2 box-border flex items-center text-md leading-6 font-bold tracking-wider py-[10px] px-2.5 rounded-lg !bg-[#FEE500] !text-[#3D1D1E]`;
-  const googleStyle = `w-full mb-2 box-border flex items-center !text-black text-md leading-6 font-bold tracking-wider py-[10px] px-2.5 rounded-lg !bg-[#fefefe]`;
-  const naverStyle = `w-full mb-2 box-border flex items-center text-md leading-6 font-bold tracking-wider py-[10px] px-2.5 rounded-lg !bg-[#2DB400]`;
+  // const googleStyle = `w-full mb-2 box-border flex items-center !text-black text-md leading-6 font-bold tracking-wider py-[10px] px-2.5 rounded-lg !bg-[#fefefe]`;
+  // const naverStyle = `w-full mb-2 box-border flex items-center text-md leading-6 font-bold tracking-wider py-[10px] px-2.5 rounded-lg !bg-[#2DB400]`;
 
   const handleNavigate = (uri: string) => {
     window.location.href = uri;
@@ -70,7 +70,7 @@ export default function LoginModal({ open, onClose }: ModalProps) {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <form className="space-y-6 mb-3" onSubmit={handleSubmit(onSubmit)}>
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 이메일
@@ -123,15 +123,7 @@ export default function LoginModal({ open, onClose }: ModalProps) {
           {/* 소셜 로그인 */}
           <button onClick={() => handleNavigate(`${KAKAO_LOGIN_URI}`)} className={kakaoStyle}>
             <RiKakaoTalkFill size={25} />
-            <span className="ms-3">카카오 로그인</span>
-          </button>
-          <button onClick={() => handleNavigate(`${GOOGLE_LOGIN_URI}`)} className={googleStyle}>
-            <FcGoogle size={25} />
-            <span className="ms-3">구글 로그인</span>
-          </button>
-          <button onClick={() => handleNavigate(`${NAVER_LOGIN_URI}`)} className={naverStyle}>
-            <SiNaver size={25} />
-            <span className="ms-3">네이버 로그인</span>
+            <span className="ms-3">카카오로 로그인하기</span>
           </button>
         </div>
       </div>
