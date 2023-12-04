ca5c7f8bda408cfe10e2d5ed9926a285ca68fa4e
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-12-04 14:16:02 +0900

    feat: 타이머 디자인 추가

--- src/pages/ZoomPage/Zoom/index.tsx
+++ src/pages/ZoomPage/Zoom/index.tsx
@@ -1,6 +1,8 @@
 import { Socket } from 'socket.io-client';
 import { GetUser } from '../type';
 import { IoPause, IoPlaySharp } from 'react-icons/io5';
+import { useEffect, useState } from 'react';
+import { useAppSelector } from '../../../store/store';
 
 interface Props {
   users: GetUser[];
@@ -8,6 +10,20 @@ interface Props {
 }
 
 export default function Zoom({ users, socket }: Props) {
+  // users에 대한 로직으로 수정 필요
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const userInfo = useAppSelector(state => state.user.userInfo);
+
+  console.log(users);
+
+  useEffect(() => {
+    if (!socket) return;
+  }, [socket]);
+
+  const handleToggleTimer = () => {
+    setIsTimerRunning(prev => !prev);
+  };
+
   return (
     <div className="h-full">
       <div className="h-full bg-[#1f1f1f]">
@@ -17,12 +33,21 @@ export default function Zoom({ users, socket }: Props) {
               <div key={user.userId} className="relative h-48 lg:h-64">
                 <div className="w-full h-full bg-[#333] border-1 border border-slate-950">
                   <div className="absolute flex items-center gap-3 right-0 bg-black text-white rounded-bl-md py-1 px-3 cursor-default">
-                    <span className="w-2 h-2 bg-lime-400 rounded-full"></span>
+                    <span className={`w-2 h-2 ${isTimerRunning ? 'bg-lime-400' : 'bg-red-500'}  rounded-full`}></span>
                     <span className="md:text-lg">00:00:00</span>
-                    <button>
-                      {/* <IoPlaySharp /> */}
-                      <IoPause />
-                    </button>
+                    {user.userId === userInfo.id && (
+                      <>
+                        {isTimerRunning ? (
+                          <button onClick={handleToggleTimer}>
+                            <IoPause />
+                          </button>
+                        ) : (
+                          <button onClick={handleToggleTimer}>
+                            <IoPlaySharp />
+                          </button>
+                        )}
+                      </>
+                    )}
                   </div>
                   <div className="w-16 h-16 rounded-full overflow-hidden relative inset-2/4 -translate-x-2/4 -translate-y-2/4">
                     <img
--- src/pages/ZoomPage/index.tsx
+++ src/pages/ZoomPage/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IoArrowBack } from 'react-icons/io5';
 import { useParams, useNavigate } from 'react-router';
 import { useAppSelector } from '../../store/store';
