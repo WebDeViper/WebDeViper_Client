cbf080f946e5b791608515720d3711e59932e0f0
seokmin <msm0748@gmail.com>
2023-12-05 21:01:06 +0900

    refactor: 백엔드 경로 수정

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,4 +1,4 @@
 import { io, Socket } from 'socket.io-client';
 
-export const socket: Socket = io('http://localhost:8001');
-export const chatSocket: Socket = io('http://localhost:8001/chat');
+export const socket: Socket = io(import.meta.env.VITE_APP_BACK_URL);
+export const chatSocket: Socket = io(`${import.meta.env.VITE_APP_BACK_URL}/chat`);
