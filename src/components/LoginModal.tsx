e3ae54f1517284991ff40b639c9c277a4abfb5c3
seokmin <msm0748@gmail.com>
2023-12-05 22:06:06 +0900

    refactor: 그룹 생성 제목 글자수 수정 및 테스트 아이디 넣기

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -66,6 +66,7 @@ export default function LoginModal({ open, onClose }: ModalProps) {
                   id="email"
                   type="email"
                   autoComplete="email"
+                  defaultValue={'test@test.com'}
                   required
                   className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   {...register('email', {
@@ -87,6 +88,7 @@ export default function LoginModal({ open, onClose }: ModalProps) {
                   id="password"
                   type="password"
                   autoComplete="current-password"
+                  defaultValue={'1234'}
                   required
                   className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   {...register('password', {
